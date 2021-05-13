85aebe91795b4b6f0e27ff72a48ac7bc7077cf9f
duong1706 <taolahieu099@gmail.com>
2021-05-14 01:23:03 +0700

    merge footer layout

--- mvc/views/detail/index.php
+++ mvc/views/detail/index.php
@@ -204,19 +204,19 @@
         <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
       </div>
       <div>
-      <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+      <a href=""><img src="<?php echo URL . 'public/style/images/cho2.jpg'; ?>" alt=""></a>
       </div>
       <div>
-      <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+      <a href=""><img src="<?php echo URL . 'public/style/images/cho3.jpg'; ?>" alt=""></a>
       </div>
       <div>
-      <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+      <a href=""><img src="<?php echo URL . 'public/style/images/cho4.jpg'; ?>" alt=""></a>
       </div>
       <div>
-      <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+      <a href=""><img src="<?php echo URL . 'public/style/images/chuot.jpg'; ?>" alt=""></a>
       </div>
       <div>
-      <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+      <a href=""><img src="<?php echo URL . 'public/style/images/meo1.jpg'; ?>" alt=""></a>
       </div>
     </div>
   </div>
@@ -227,109 +227,28 @@
     </div>
     <div class="regular slider">
         <div>
-        <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+        <a href=""><img src="<?php echo URL . 'public/style/images/meo2.jpg'; ?>" alt=""></a>
         </div>
         <div>
-        <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+        <a href=""><img src="<?php echo URL . 'public/style/images/meo3.jpg'; ?>" alt=""></a>
         </div>
         <div>
-        <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+        <a href=""><img src="<?php echo URL . 'public/style/images/meo4.jpg'; ?>" alt=""></a>
         </div>
-        <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+        <a href=""><img src="<?php echo URL . 'public/style/images/x5.jpg'; ?>" alt=""></a>
         <div>
-        <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+        <a href=""><img src="<?php echo URL . 'public/style/images/x7.jpg'; ?>" alt=""></a>
         </div>
         <div>
-        <a href=""><img src="<?php echo URL . 'public/style/images/cho1.jpg'; ?>" alt=""></a>
+        <a href=""><img src="<?php echo URL . 'public/style/images/x9.jpg'; ?>" alt=""></a>
         </div>
     </div>
 
-    <div id="footer">
-      <div class="row" id="foot1">
-        <div class="col-md-6 social">
-          <div class="contain">
-            <a href="#"><i class="fab fa-facebook-f"></i></a>
-            <a href="#"><i class="fab fa-twitter"></i></a>
-            <a href="#"><i class="fab fa-google-plus-g"></i></a>
-          </div>
-          <span></span>
-        </div>
-        <div class="col-md-6 payment">
-          <span></span>
-          <div class="contain">
-            <i class="fab fa-cc-paypal"></i>
-            <i class="fab fa-cc-visa"></i>
-            <i class="fab fa-cc-mastercard"></i>
-          </div>
-        </div>
+    <div>
+          <?php
+            require_once './mvc/views/hf/footer.php';
+          ?>
       </div>
-      <div class="row foot2">
-        <div class="col-lg-1 col-md-0 col-sm-0 col-xs-0"></div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
-          
-          <ul>
-            <li><h5>About us</h5></li>
-            <li><a href="#">Careers</a></li>
-            <li><a href="#">Company Information</a></li>
-            <li><a href="#">News Room</a></li>
-            <li><a href="#">Compliance & Ethics</a></li>
-            <li><a href="#">Site Map</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
-          
-          <ul>
-            <li><h5>Education</h5></li>
-            <li><a href="#">Food Center</a></li>
-            <li><a href="#">Care Guides</a></li>
-            <li><a href="#">Puppy Center</a></li>
-            <li><a href="#">Fish Center</a></li>
-            <li><a href="#">Small Pet Center</a></li>
-            <li><a href="#">Bird Center</a></li>
-            <li><a href="#">Reptile Center</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
-          
-          <ul>
-            <li><h5>Popular</h5></li>
-            <li><a href="#">Dog & Cat</a></li>
-            <li><a href="#">Reptile</a></li>
-            <li><a href="#">Fish & Bird</a></li>
-            <li><a href="#">Sale Off</a></li>
-            <li><a href="#">Small Pet</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
-          
-          <ul>
-            <li><h5>Services</h5></li>
-            <li><a href="#">Pet Camp</a></li>
-            <li><a href="#">Pet Hotel Boarding</a></li>
-            <li><a href="#">Pet Grooming</a></li>
-            <li><a href="#">Dog Training</a></li>
-            <li><a href="#">Banfield Vet Services</a></li>
-            <li><a href="#">Pet Expressions</a></li>
-          </ul>
-        </div>
-        <div class="col-lg-2 col-md-4 col-sm-4 col-xs-12">
-          <h5>Subscribe</h5>
-          <p>Subscribe to Our Weekly Newsletter</p>
-          <form action="">
-            <input type="email" placeholder="email@domain.com" id="email">
-          </form>
-          <a href="#" id="submit">Submit</a>
-        </div>
-        <div class="col-lg-1 col-md-0 col-sm-0 col-xs-0"></div>
-    
-        <div class="info">
-          <hr>
-          <a href="#">© 2017 Best Friend.</a>
-          <span>All rights reserved</span>
-        </div>
-      </div>
-      
-    </div>
   </div>
   </div>
 

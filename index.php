66774d78ff8ecc9c2a7a08f279f5bc1d91c87ff9
anh753159 <nguyenlam.240500@gmail.com>
2021-05-28 17:40:16 +0700

    28/5/2021

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 session_start();
 $_SESSION['lct'] = "0";
 //unset($_SESSION['cart']);
-// echo $_GET['url']; // lấy đường dẫn về url = abc thif echo se in ra abc
+// echo $_GET['url']; //////////// lấy đường dẫn về url = abc thif echo se in ra abc
 require_once "./mvc/Bridge.php";
 $myapp = new App();
 
